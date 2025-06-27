1b0fa110118997c417b0290eee135fd04e26fd4b
Pranto Paul <prantopaul55555@gmail.com>
2025-06-28 03:49:49 +0600

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+const page = () => {
+  return (
+    <div>
+        <Button variant={"outline"}>Hello there!</Button>
+    </div>
+  )
+}
+export default page
